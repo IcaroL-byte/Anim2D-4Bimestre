f3efc85b78cbeab761b8aba62845c7fd46fa1813
IcaroL-byte <icaro.lauan@escolar.ifrn.edu.br>
2026-02-03 22:31:05 -0300

    Flip do personagem

--- TrabalhoDeAnimação2D/Assets/Scripts/Movement.cs
+++ TrabalhoDeAnimação2D/Assets/Scripts/Movement.cs
@@ -3,11 +3,14 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     public int speed;
-    
+    private bool isFacingRight;
+    private SpriteRenderer srPlayer;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        isFacingRight = true;
+        srPlayer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -20,10 +23,21 @@ public class Movement : MonoBehaviour
     {
         if(Input.GetKey(KeyCode.D)){
             gameObject.transform.position += new Vector3(speed * Time.deltaTime, 0f, 0f);
+            if (!isFacingRight)
+            {
+                srPlayer.flipX = false;
+                isFacingRight = true;
+            }
+
         }
 
          if(Input.GetKey(KeyCode.A)){
             gameObject.transform.position -= new Vector3(speed * Time.deltaTime, 0f, 0f);
+            if (isFacingRight)
+            {
+                srPlayer.flipX = true;
+                isFacingRight = false;
+            }
         }
     }
 
